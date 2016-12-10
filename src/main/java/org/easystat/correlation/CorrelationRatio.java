5f329d78d67b340486fec49ca9536786cf4fa11e
vkmbox <vkmbox@yandex.ru>
2016-12-10 21:47:11 +0300

    Correlation ratio realization

---
+++ src/main/java/org/easystat/correlation/CorrelationRatio.java
@@ -0,0 +1,84 @@
+package org.easystat.correlation;
+
+import java.util.*;
+import org.easystat.util.QuickSort;
+
+import org.apache.commons.math4.stat.StatUtils;
+import org.apache.commons.math4.exception.util.LocalizedFormats;
+import org.apache.commons.math4.exception.MathIllegalArgumentException;
+
+public class CorrelationRatio {
+
+  /**
+   * Computes the correlation ratio between the two arrays. 
+   * It is expected that in the first array are found duplicates.
+   *
+   * <p>Array lengths must match and the common length must be at least 2.</p>
+   *
+   * @param xArray first data array
+   * @param yArray second data array
+   * @return returns the correlation ratio for the two arrays
+   * @throws  MathIllegalArgumentException if the arrays lengths do not match or
+   * there is insufficient data
+   */
+  public double correlationRatio(final double[] xArray, final double[] yArray) throws MathIllegalArgumentException 
+  {
+    int length = xArray.length;
+    if ( length != yArray.length ) {
+      throw new MathIllegalArgumentException( LocalizedFormats.DIMENSIONS_MISMATCH_SIMPLE, length, yArray.length );
+    } else if ( length < 2 ) {
+      throw new MathIllegalArgumentException( LocalizedFormats.INSUFFICIENT_OBSERVED_POINTS_IN_SAMPLE, length, 2 );
+    }
+
+    //toList(xArray);
+    //toList(yArray);
+    
+    QuickSort sorter = new QuickSort() {
+      protected void swapDouble( double[] array, int posA, int posB ) 
+      {
+        super.swapDouble( xArray, posA, posB );
+        super.swapDouble( yArray, posA, posB );
+      }
+    };
+    
+    sorter.qsortDouble(xArray);
+    
+    //toList(xArray);
+    //toList(yArray);
+    
+    double currX = xArray[0];
+    int currLen = 0, currPos = 0;
+    double yMean = StatUtils.mean(yArray);
+    double regSum = 0d, varSum = 0d;
+    
+    for ( int ii = 0; ii < length; ii++ )
+    {
+      varSum += Math.pow(( yMean - yArray[ii] ),2);
+      if ( currX == xArray[ii] ) {
+        currLen += 1;
+      } else {
+        //System.out.println(currPos + ";" + currLen + ";" + StatUtils.mean(yArray, currPos, currLen));
+        regSum += currLen*Math.pow(( yMean - StatUtils.mean(yArray, currPos, currLen) ),2);
+        currX = xArray[ii]; currLen = 1; currPos = ii;
+      }
+    }
+    //System.out.println(currPos + ";" + currLen + ";" + StatUtils.mean(yArray, currPos, currLen));
+    regSum += currLen*Math.pow(( yMean - StatUtils.mean(yArray, currPos, currLen) ),2);
+    
+    //System.out.print(regSum+";"+varSum+";"+regSum/varSum);
+    return varSum == 0 ? Double.NaN : Math.pow(regSum/varSum, 0.5);
+  }
+  
+  /*private List<Double> toList( double[] val )
+  {
+    List<Double> result = new ArrayList<>(val.length);
+    for ( int ii = 0; ii < val.length; ii++ )
+    {
+      //System.out.print(val[ii]+";");
+      val[ii] = new Double(val[ii]);
+    }
+    System.out.println("");
+    return result;
+  }*/
+  
+}
---
+++ src/main/java/org/easystat/util/QuickSort.java
@@ -0,0 +1,42 @@
+package org.easystat.util;
+
+import java.util.Random;
+
+public class QuickSort {
+  
+  public static final Random RND = new Random();
+  
+  protected void swapDouble( double[] array, int posA, int posB ) 
+  {
+    double tmp = array[posA];
+    array[posA] = array[posB];
+    array[posB] = tmp;
+  }
+  
+  private int partitionDouble( double[] array, int begin, int end ) 
+  {
+    int index = begin + RND.nextInt(end - begin + 1);
+    double pivot = array[index];
+    swapDouble( array, index, end );
+    for (int ii = index = begin; ii < end; ++ ii) {
+      if ( array[ii] <= pivot ) {
+        swapDouble(array, index++, ii);
+      }
+    }
+    swapDouble(array, index, end);
+    return index;
+  }
+  
+  private void qsortDouble( double[] array, int begin, int end )
+  {
+    if ( end > begin ) {
+      int index = partitionDouble(array, begin, end );
+      qsortDouble(array, begin, index - 1 );
+      qsortDouble(array, index + 1,  end );
+    }
+  }
+    
+  public void qsortDouble( double[] array ) {
+        qsortDouble( array, 0, array.length - 1 );
+  }
+}
